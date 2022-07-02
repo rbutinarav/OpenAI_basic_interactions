50fb5dc5ea9310acb6ec4ac49ace5188e163bd98
Roberto Butinar <roberto.butinar@avanade.com>
2022-07-02 19:40:07 +0200

    prompt recoded with def function and other improvements and extensions

---
+++ export_to_csv.py
@@ -0,0 +1,16 @@
+import os
+import openai
+import json
+from dotenv import load_dotenv
+import pandas as pd
+
+#EXPORT TO CSV
+prompt_openAI="Il migliore amico dell'uomo è"
+response_text="Il cane, il 'gatto'"
+
+#EXPORT TO CSV
+prompt_response={'prompt':[prompt_openAI],'completion': [response_text]}
+prompt_response_df = pd.DataFrame(data=prompt_response)
+
+prompt_response_df.to_csv('out.zip', encoding="utf-8")
+print(prompt_response_df)
---
+++ prompt_requests.py
@@ -0,0 +1,44 @@
+def ai_complete(prompt='Hello', model='text-davinci-002', temperature=0.2, max_tokens=6, verbose=False):
+    """
+    Returns a string with the completion of the prompt.
+    """
+    import os
+    import openai
+    import json
+    from dotenv import load_dotenv
+    import pandas as pd
+
+    #LOAD ENV VARIABLES
+    load_dotenv()
+    openai.api_key = os.getenv('OPENAI_KEY')
+
+    response = openai.Completion.create(model=model, prompt=prompt, temperature=temperature, max_tokens=max_tokens)
+    response_str=str(response) #converte oggetto OpenAI in str
+    response_dict=json.loads(response_str) #converte la stringa formattata in json in variabile dict
+    response_text = response_dict['choices'][0]['text'] #estrae la completion (choices/0/text)
+    if verbose == True:
+        return prompt, response_text
+    return response_text
+
+def ai_training ():
+    """
+    Train a model from json dataset and return the model name.
+    """
+    import os
+    import openai
+    import json
+    from dotenv import load_dotenv
+    import pandas as pd
+
+    #LOAD ENV VARIABLES
+    load_dotenv()
+    openai.api_key = os.getenv('OPENAI_KEY')
+
+    return ('This is a test')
+
+#THESE ARE EXAMPLES OF USE OF THE AI_COMPLETE FUNCTION
+print(ai_complete("Il migliore amico dell'uomo è", verbose=True))
+print(ai_complete("Il più grande musicista di tutti i tempi è",verbose=True))
+
+
+
