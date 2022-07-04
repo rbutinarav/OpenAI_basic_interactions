8c0fb182a93d3329beb4c88110e28bf49dd6ceb7
Roberto Butinar <roberto.butinar@avanade.com>
2022-07-04 22:32:54 +0200

    --

--- 1.use_simple_prompts.py
+++ 1.use_simple_prompts.py
@@ -1,6 +1,6 @@
 from openai_functions import ai_complete
 
-#EXAMPLE 1: SIMPLE USE OF ai_complete FUNCTION WITH SIMPLE PROMPT-
+#EXAMPLE 1: SIMPLE USE OF ai_complete FUNCTION WITH SIMPLE PROMPT
 
 print(ai_complete("Il migliore amico dell'uomo è", verbose=True))
 print(ai_complete("Il più grande musicista di tutti i tempi è",verbose=True))
