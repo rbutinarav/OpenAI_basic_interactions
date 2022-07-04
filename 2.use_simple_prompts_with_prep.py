59946dcab82eb37d1f066e97fd198cdf74c81b76
Roberto Butinar <roberto.butinar@avanade.com>
2022-07-04 22:13:54 +0200

    final 4.7.2022

---
+++ 2.use_simple_prompts_with_prep.py
@@ -0,0 +1,12 @@
+from openai_functions import ai_complete
+
+#EXAMPLE 2: USE OF ai_complete WITH A PROMPT AND A TRAINING TEXT USED TO EXTEND THE PROMPT (RESTAURANT REVIEWS)
+
+prompt="Review: Locale molto accogliente, ampia selezione di vini, menù con poche portate ma molto ricercate e buone, prezzi alti ma in linea con la qualità del cibo e del servizio."
+
+with open ('prep_reviews_json.txt', 'r') as f:
+    prep = f.read()
+
+extended_prompt=prep+'\n'+prompt
+
+print('REVIEW:\n',prompt,'\n\nTEXT ANALYICS:', ai_complete(extended_prompt, max_tokens=200))
